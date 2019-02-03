03ce45765d4261bfb32f1ced57042607db40837d
mchll-ln <michellelin6492@hotmail.com>
2019-02-02 22:51:43 -0500

    Update eventPage.js

--- eventPage.js
+++ eventPage.js
@@ -12,3 +12,32 @@ chrome.contextMenus.onClicked.addListener(function(request){
 		alert('success! Url: ' + imgSrc);  // for testing
 	}
 })
+
+function processText(sourceImageUrl) {
+			 var params = {
+					 // Request parameters
+					 "language": "unk",
+					 "detectOrientation ": "true",
+			 };
+
+			 $.ajax({
+					 url: "https://westus.api.cognitive.microsoft.com/vision/v2.0/ocr?" + $.param(params),
+					 beforeSend: function(xhrObj){
+							 // Request headers
+							 xhrObj.setRequestHeader("Content-Type","application/json");
+							 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","3d4299781c9f4fc394ef6ede4a429325");
+					 },
+					 type: "POST",
+					 // Request body
+					 data: '{"url": ' + '"' + sourceImageUrl + '"}',
+			 })
+
+			 .done(function(data) {
+					 alert("success");
+			 })
+			 .fail(function() {
+					 alert("error");
+			 });
+
+			 alert('abcd');
+	 };
